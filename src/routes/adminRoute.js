21a8c32ad014e0113698be73a1f479f9110c02eb
Namchanut <chanut39@hotmail.com>
2023-06-23 19:05:34 +0700

    edit

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -5,7 +5,7 @@ const adminController = require("../controllers/adminController");
 const router = express.Router();
 
 router.post("/create-pricing-plan", adminController.createPricingPlan);
-router.get("/pricing-plan", adminController.getAllPricingPlanList);
-router.get("/pricing-plan/:id", adminController.getPricingPlanList);
+router.get("/pricing-plan", adminController.getAllPricingPlan);
+router.get("/pricing-plan/:id", adminController.getPricingPlanById);
 
 module.exports = router;
---
+++ src/services/adminService.js
@@ -0,0 +1,8 @@
+const { token } = require("morgan");
+const adminRepository = require("../repositories/adminRepository");
+
+exports.createPackage = (package) => adminRepository.createPackage(package);
+
+exports.getPackageById = (id) => adminRepository.getPackageById(id);
+
+exports.getAllPricingPlan = () => adminRepository.getAllPricingPlan();
